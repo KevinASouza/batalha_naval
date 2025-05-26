6803d2db2604474a5c5f3fd05fbca89e32ffe262
Kevin Assunção Souza <99425522+KevinASouza@users.noreply.github.com>
2025-05-26 20:27:46 -0300

    Primeiro commit

---
+++ batalha_naval.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// Variáveis constantes
+#define LINHAS 10
+#define COLUNAS 10
+#define TAMANHO_NAVIO 3
+
+// Função principal
+int main()
+{
+        // Matriz que simula um tabuleiro para o jogo batalha naval
+        int tabuleiro[LINHAS][COLUNAS] = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0}
+        };
+
+        // Variáveis que simulam os navios do jogo
+        int navio_horizontal[TAMANHO_NAVIO] = {3, 3, 3};
+        int navio_vertical[TAMANHO_NAVIO] = {3, 3, 3};
+
+        // Posicionando o navio no tabuleiro usando indices fixos
+        tabuleiro[0][0] = navio_horizontal[0];
+        tabuleiro[0][1] = navio_horizontal[1];
+        tabuleiro[0][2] = navio_horizontal[2];
+        tabuleiro[7][9] = navio_vertical[0];
+        tabuleiro[8][9] = navio_vertical[1];
+        tabuleiro[9][9] = navio_vertical[2];
+
+        // Loop para percorrer os valores do tabuleiro
+        for (size_t i = 0; i < LINHAS; i++)
+        {
+            for (size_t j = 0; j < COLUNAS; j++)
+            {
+                printf("%d  ", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+        
+    return 0;
+}
