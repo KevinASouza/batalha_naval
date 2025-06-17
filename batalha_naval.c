16b5a9f014795d43ec4d126ff053fbccaa17a0d1
Kevin Assunção Souza <99425522+KevinASouza@users.noreply.github.com>
2025-06-17 20:12:03 -0300

    Inserir mais navios

--- batalha_naval.c
+++ batalha_naval.c
@@ -25,15 +25,23 @@ int main()
         // Variáveis que simulam os navios do jogo
         int navio_horizontal[TAMANHO_NAVIO] = {3, 3, 3};
         int navio_vertical[TAMANHO_NAVIO] = {3, 3, 3};
+        int navio1_diagonal[TAMANHO_NAVIO] = {3, 3, 3};
+        int navio2_diagonal[TAMANHO_NAVIO] = {3, 3, 3};
 
-        // Posicionando o navio no tabuleiro usando indices fixos
+        // Posicionando o navio no tabuleiro usando indices literais
         tabuleiro[0][0] = navio_horizontal[0];
         tabuleiro[0][1] = navio_horizontal[1];
         tabuleiro[0][2] = navio_horizontal[2];
-        tabuleiro[7][9] = navio_vertical[0];
-        tabuleiro[8][9] = navio_vertical[1];
-        tabuleiro[9][9] = navio_vertical[2];
-
+        tabuleiro[5][9] = navio_vertical[0];
+        tabuleiro[6][9] = navio_vertical[1];
+        tabuleiro[7][9] = navio_vertical[2];
+        tabuleiro[2][2] = navio1_diagonal[0];
+        tabuleiro[3][3] = navio1_diagonal[1];
+        tabuleiro[4][4] = navio1_diagonal[2];
+        tabuleiro[7][7] = navio2_diagonal[0];
+        tabuleiro[8][8] = navio2_diagonal[1];
+        tabuleiro[9][9] = navio2_diagonal[2];
+ 
         // Loop para percorrer os valores do tabuleiro
         for (size_t i = 0; i < LINHAS; i++)
         {
